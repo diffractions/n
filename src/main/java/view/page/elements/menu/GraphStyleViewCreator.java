3cc55b3f96a4a89955be4c6b877424db849ba1cb
Andri Work <andri.kozak@gmail.com>
2016-10-06 12:51:23 +0300

    modify view

--- src/main/java/view/page/elements/menu/GraphStyleViewCreator.java
+++ src/main/java/view/page/elements/menu/GraphStyleViewCreator.java
@@ -13,42 +13,57 @@ import entity.Table;
 import view.page.PageManager;
 import view.page.ViewCreator;
 import view.page.elements.graph.GraphRootPnaelUpdater; 
+import java.util.Arrays;
+
+import javax.swing.border.TitledBorder;
+
 
 public class GraphStyleViewCreator implements ViewCreator {
 
-  final JComboBox<?> combobox1 = new JComboBox<>(new String[]{"0","1","2","3","4","5","6","7","8","9","10"});  
-  final JComboBox<?> combobox2 = new JComboBox<>(new String[]{"0","1","2","3","4","5","6","7","8","9","10"});  
- // final GraphRootPnaelUpdater ml1 = null;
+
+ // final JComboBox<Integer> combobox1 = new JComboBox<>(new Integer[]{0,1,2,3,4,5,6,7,8,9,10});  
+  final Integer[] dropPoint =  new Integer[]{1,2,3,4,5,6,7,8,9,10};
+   final JComboBox<Integer> combobox2 = new JComboBox<>(dropPoint);    
 
 	@Override
 	public JComponent getView(final Table table, ModifyTable... tables) {
 		JPanel p = new JPanel();
-		p.setBackground(Color.BLACK);
+		p.setBackground(Color.GREEN);
+		
+		
 		
 		//ml1 = new GraphRootPnaelUpdater (table);
 		int i1  = (int) PageManager.tableLineView.get(table).getDiameter(1);
-		int i2  = (int) PageManager.tableLineView.get(table).getDropPoint();
+		int i2  = Arrays.binarySearch(dropPoint,(int) PageManager.tableLineView.get(table).getDropPoint());
 		
-		
-		combobox1.setSelectedIndex(i1); 
-		combobox1.addItemListener(new ItemListener() {
-			public void itemStateChanged(ItemEvent e) {
-				PageManager.tableLineView.get(table).setDiameter(1, combobox1.getSelectedIndex());
-				new GraphRootPnaelUpdater (table).repaint();
-			}
-		});
+
+		for(int i = 1; i< table.getCollCount(); i++){
+  			final JComboBox<Integer> combobox1 = new JComboBox<>(new Integer[]{0,1,2,3,4,5,6,7,8,9,10}); 
+			combobox1.setBorder(new TitledBorder(table.getHeaders()[i]));
+			final int size = i;
+			combobox1.setSelectedIndex(i1); 
+
+			combobox1.addItemListener(new ItemListener() {
+				public void itemStateChanged(ItemEvent e) {
+					PageManager.tableLineView.get(table).setDiameter(size, combobox1.getSelectedIndex());
+					new GraphRootPnaelUpdater (table).repaint();
+				}
+			});
+
+			p.add(combobox1);
+		}
 
 		combobox2.setSelectedIndex(i2); 
 		combobox2.addItemListener(new ItemListener() {
 			public void itemStateChanged(ItemEvent e) {
-				PageManager.tableLineView.get(table).setDropPoint( combobox2.getSelectedIndex());
+				PageManager.tableLineView.get(table).setDropPoint( dropPoint[combobox2.getSelectedIndex()]);
 				new GraphRootPnaelUpdater (table).repaint();
 			}
 		});
 
 
 
-		p.add(combobox1);
+		//p.add(combobox1);
 		p.add(combobox2);
 		return p;
 	}
